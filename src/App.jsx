885367e86eae0c616705d9141879349002e85d54
jeevana4587 <jeevanapavuluru4587@gmail.com>
2026-01-23 21:42:06 +0530

    Merge branch 'dev' into 'room'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing-page.jsx";
+import JoinRoom from "./pages/Join- room.jsx";
+import VideoCall from "./pages/VideoCall";
+import HandTrackingDemo from "./pages/HandTrackingDemo";
+
+
 import Room from "./pages/Room";
 
 function App() {
   return <Room />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/join" element={<JoinRoom />} />
+        <Route path="/call" element={<VideoCall />} />
+        <Route path="/hand-demo" element={<HandTrackingDemo />} />
+
+
+        {/* <Route path="/call" element={<VideoCall />} />
+        <Route path="/hand-demo" element={<HandTrackingDemo />} />  */}
+      </Routes>
+    </BrowserRouter>
+    
+  );
 }
 
 export default App;
