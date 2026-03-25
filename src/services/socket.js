4a2d03391f7e14bb4f7dd866dd15be419a35cb37
jeevana4587 <jeevanapavuluru4587@gmail.com>
2026-03-25 14:21:34 +0530

    added extra headers

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = " https://unbadgered-junie-cyclostomatous.ngrok-free.dev";
 
+const SOCKET_SERVER_URL = (" https://unbadgered-junie-cyclostomatous.ngrok-free.dev",{
+extraHeaders: {
+    "ngrok-skip-browser-warning": "true"
+  }
+});
 export const socket = io(SOCKET_SERVER_URL, {
   autoConnect: false
 });
