247de987411b2d4e38e573b698bad8eba23364e8
jeevana4587 <jeevanapavuluru4587@gmail.com>
2026-03-25 15:03:04 +0530

    changes in vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // This ensures all assets are loaded from the root
   server: {
-    host: true, // <-- allows access from local network
-    port: 5173, // optional, default port
-    base:'./',
+    host: true,
+    port: 5173,
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 })
