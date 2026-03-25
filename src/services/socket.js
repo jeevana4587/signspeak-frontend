c0ba9e7ce0c263cd76fa4d1799b8a00915789fdd
jeevana4587 <jeevanapavuluru4587@gmail.com>
2026-03-25 14:32:40 +0530

    made changes in socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
 
+// Use a simple string for the URL
+const URL = "https://unbadgered-junie-cyclostomatous.ngrok-free.dev";
 
-const SOCKET_SERVER_URL = (" https://unbadgered-junie-cyclostomatous.ngrok-free.dev",{
-extraHeaders: {
+// Combine the ngrok header and the autoConnect setting into one object
+export const socket = io(URL, {
+  autoConnect: false,
+  extraHeaders: {
     "ngrok-skip-browser-warning": "true"
   }
-});
-export const socket = io(SOCKET_SERVER_URL, {
-  autoConnect: false
 });
