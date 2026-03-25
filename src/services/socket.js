586ea164e173f8161685572e02cb6bc52820d644
jeevana4587 <jeevanapavuluru4587@gmail.com>
2026-03-25 14:09:04 +0530

    changed ngork link

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "https://groutiest-groundlessly-catheryn.ngrok-free.dev";
+const SOCKET_SERVER_URL = " https://unbadgered-junie-cyclostomatous.ngrok-free.dev";
 
 export const socket = io(SOCKET_SERVER_URL, {
   autoConnect: false
