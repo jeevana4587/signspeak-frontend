4147464878910e716ac4c356354837282637c60e
jeevana4587 <jeevanapavuluru4587@gmail.com>
2026-04-02 23:53:04 +0530

    made changes in code regarding imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import GesturePredict from "./pages/GesturePredict";
+// import GesturePredict from "./pages/GesturePredict";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing-page.jsx";
 import JoinCall from "./pages/Join-room.jsx";
-import VideoCall from "./pages/VideoCall";
-import HandTrackingDemo from "./pages/HandTrackingDemo";
+// import VideoCall from "./pages/VideoCall";
+// import HandTrackingDemo from "./pages/HandTrackingDemo";
 import Room from "./pages/Room";
 import CreateCall from "./pages/CreateCall.jsx";
 
@@ -13,11 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/join" element={<JoinCall />} />
-        <Route path="/call" element={<VideoCall />} />
+        {/* <Route path="/call" element={<VideoCall />} /> */}
         <Route path="/create" element={<CreateCall />} />
-        <Route path="/hand-demo" element={<HandTrackingDemo />} />
+        {/* <Route path="/hand-demo" element={<HandTrackingDemo />} /> */}
         <Route path="/room/:roomId" element={<Room />} />
-        <Route path="/gesture-predict" element={<GesturePredict />} />
+        {/* <Route path="/gesture-predict" element={<GesturePredict />} /> */}
       </Routes>
     </BrowserRouter>
   );
